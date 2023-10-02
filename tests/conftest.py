b65b61bd3c1495d59580b312601a5c31e6bc5036
Anton Caceres <anton@caceres.me>
2023-10-02 17:01:27 +0200

    test: add RedisXreadMock to fix missing support of fakeredis protocol RESP3 for xread

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,66 @@
 import pytest
 from fakeredis import aioredis
+from redis._parsers.helpers import (
+    parse_xread_resp3,
+    string_keys_to_dict,
+    bool_ok,
+    parse_command_resp3,
+    parse_sentinel_state_resp3,
+    parse_sentinel_masters_resp3,
+    parse_sentinel_slaves_and_sentinels_resp3,
+)
+from redis.utils import str_if_bytes
+
 from dispatcher.main import app
 from dispatcher.persistence.redis import redis_dependency
 
 
 async def redis_fake_dependency():
-    connection = aioredis.FakeRedis(decode_responses=True, protocol=3)
+    connection = aioredis.FakeRedis(decode_responses=True, protocol=2)
+    connection.response_callbacks.update(
+        {
+            # **string_keys_to_dict(
+            #     "ZRANGE ZINTER ZPOPMAX ZPOPMIN ZRANGEBYSCORE ZREVRANGE ZREVRANGEBYSCORE "
+            #     "ZUNION HGETALL XREADGROUP",
+            #     lambda r, **kwargs: r,
+            # ),
+            **string_keys_to_dict("XREAD XREADGROUP", parse_xread_resp3),
+            "ACL LOG": lambda r: [
+                {str_if_bytes(key): str_if_bytes(value) for key, value in x.items()}
+                for x in r
+            ]
+            if isinstance(r, list)
+            else bool_ok(r),
+            "COMMAND": parse_command_resp3,
+            "CONFIG GET": lambda r: {
+                str_if_bytes(key)
+                if key is not None
+                else None: str_if_bytes(value)
+                if value is not None
+                else None
+                for key, value in r.items()
+            },
+            "MEMORY STATS": lambda r: {
+                str_if_bytes(key): value for key, value in r.items()
+            },
+            "SENTINEL MASTER": parse_sentinel_state_resp3,
+            "SENTINEL MASTERS": parse_sentinel_masters_resp3,
+            "SENTINEL SENTINELS": parse_sentinel_slaves_and_sentinels_resp3,
+            "SENTINEL SLAVES": parse_sentinel_slaves_and_sentinels_resp3,
+            "STRALGO": lambda r, **options: {
+                str_if_bytes(key): str_if_bytes(value) for key, value in r.items()
+            }
+            if isinstance(r, dict)
+            else str_if_bytes(r),
+            "XINFO CONSUMERS": lambda r: [
+                {str_if_bytes(key): value for key, value in x.items()} for x in r
+            ],
+            "XINFO GROUPS": lambda r: [
+                {str_if_bytes(key): value for key, value in d.items()} for d in r
+            ],
+        }
+    )
+
     try:
         yield connection
     finally:
