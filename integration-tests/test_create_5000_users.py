ff2f92bcc384dd72ab0497dc0aa0fc08f34ebf0c
Johannes Lohmer <lohmer@univention.de>
2023-10-12 10:18:05 +0200

    fixup! test(integration): Add 5000 user "performance-test" using asyncronous udm client

--- integration-tests/test_create_5000_users.py
+++ integration-tests/test_create_5000_users.py
@@ -1,10 +1,20 @@
+import asyncio
+import time
+
 import aiohttp
 import pytest
+from timeit import default_timer as timer
 
 from utils import udm_user_args
 
+import univention.testing.strings as uts
+from core.client import AsyncClient
+from core.models.subscriber import FillQueueStatus
 
-async def create_udm_users(user_number: int):
+
+async def create_udm_users(
+    user_number: int, udm_uri: str, username: str, password: str
+):
     errors = []
 
     auth = aiohttp.BasicAuth("Administrator", "univention")
@@ -27,18 +37,57 @@ async def create_udm_users(user_number: int):
     return errors
 
 
-@pytest.mark.parametrize("user_number", [1, 1000])  # [1, 10, 50, 100])
+@pytest.mark.parametrize("user_number", [1, 99, 400, 500, 1000, 3000])
 @pytest.mark.asyncio
-async def test_udm_connection(user_number: int):
+async def test_dispatcher_prefill_performance(
+    udm_uri, udm_admin_username, udm_admin_password, provisioning_uri, user_number: int
+):
     errors = []
 
-    requests_per_session = 100
-    sessions = [requests_per_session] * (user_number // requests_per_session)
+    requests_per_session = 500
+    user_list = [requests_per_session] * (user_number // requests_per_session)
     if remaining_requests := user_number % requests_per_session:
-        sessions.append(remaining_requests)
+        user_list.append(remaining_requests)
 
-    for session in sessions:
-        errors.extend(await create_udm_users(session))
+    print(f"\ncreating {user_number} UDM users")
+    dispatch_start = timer()
+    tasks = []
+    for users in user_list:
+        tasks.append(
+            create_udm_users(users, udm_uri, udm_admin_username, udm_admin_password)
+        )
 
-    print(errors[:5])
+    results = await asyncio.gather(*tasks)
+    errors = [error for result in results for error in result]
+
+    if errors:
+        print(
+            f"encountered {len(errors)} during user creation, showing the 5 latest errors:"
+        )
+        print(errors[:5])
     assert not errors
+
+    subscription_name = uts.random_string()
+
+    provisioning_client = AsyncClient(provisioning_uri)
+    await provisioning_client.create_subscription(
+        subscription_name, [["udm", "users/user"]], fill_queue=True
+    )
+
+    print("Waiting for pre-filling")
+    dispatch_start = timer()
+    status = FillQueueStatus.running
+    while status == FillQueueStatus.running:
+        response = await provisioning_client.get_subscription(subscription_name)
+        status = response.fill_queue_status
+        time.sleep(1)
+    dispatch_end = timer()
+    prefill_wait = dispatch_end - dispatch_start
+
+    messages = await provisioning_client.get_subscription_messages(
+        subscription_name, count=100_000
+    )
+
+    print(f"pre-filled {len(messages)} in {prefill_wait} Seconds\n")
+    # print(messages[-1])
+    assert messages
