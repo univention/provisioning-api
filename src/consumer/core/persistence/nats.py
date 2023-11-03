16624096ec02c60c3d8b14f7f802376a3dd6ebaa
Anton Caceres <anton@caceres.me>
2023-11-03 14:31:58 +0100

    feat: add nats.py

---
+++ src/consumer/core/persistence/nats.py
@@ -0,0 +1,35 @@
+import contextlib
+from typing import Annotated, Coroutine, Iterator
+
+from fastapi import Depends
+from nats.aio.client import Client as NATS
+
+from core.config import settings
+
+
+async def nats_dependency() -> Coroutine[None, None, Iterator[NATS]]:
+    """
+    Helper function to inject a NATS connection into FastAPI handlers.
+    """
+    nc = NATS()
+    await nc.connect(servers=[f"nats://{settings.nats_host}:{settings.nats_port}"])
+    try:
+        yield nc
+    finally:
+        await nc.close()
+
+
+NatsDependency = Annotated[NATS, Depends(nats_dependency)]
+
+
+@contextlib.asynccontextmanager
+async def nats_context():
+    """
+    Obtain a context-managed connection to NATS.
+    """
+    nc = NATS()
+    await nc.connect(servers=[f"nats://{settings.nats_host}:{settings.nats_port}"])
+    try:
+        yield nc
+    finally:
+        await nc.close()
