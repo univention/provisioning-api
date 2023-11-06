382224dca7d40146029841daacc29f4eb172fa74
Anton Caceres <anton@caceres.me>
2023-11-06 15:09:46 +0200

    feat: add MessageRepository logic to port and adapter

--- src/consumer/messages/persistence/messages.py
+++ src/consumer/messages/persistence/messages.py
@@ -1,4 +1,4 @@
-from typing import Annotated, Dict, cast, List, Optional, Tuple
+from typing import Annotated, List, Optional, Tuple
 
 import core.models
 import fastapi
@@ -46,15 +46,11 @@ class MessageRepository:
         :param str subscriber_name: Name of the subscriber.
         :param core.models.Message message: The message from the pre-fill task.
         """
-
-        key = Keys.queue(subscriber_name)
-        flat_message = message.flatten()
-        await self.redis.xadd(key, flat_message, "0-*")
+        await self.port.add_prefill_message(subscriber_name, message)
 
     async def delete_prefill_messages(self, subscriber_name: str):
         """Delete all pre-fill messages from the subscriber's queue."""
-        key = Keys.queue(subscriber_name)
-        await self.redis.xtrim(key, minid=1)
+        await self.port.delete_prefill_messages(subscriber_name)
 
     async def get_next_message(
         self, subscriber_name: str, block: Optional[int] = None
@@ -64,19 +60,7 @@ class MessageRepository:
         :param str subscriber_id: Id of the subscriber.
         :param int block: How long to block in milliseconds if no message is available.
         """
-        key = Keys.queue(subscriber_name)
-
-        response = await self.redis.xread({key: "0-0"}, count=1, block=block)
-
-        if key not in response:
-            # empty stream
-            return None
-
-        entries = response[key][0]
-        if entries:
-            message_id, flat_message = cast(Tuple[str, Dict[str, str]], entries[0])
-            message = core.models.Message.inflate(flat_message)
-            return (message_id, message)
+        return await self.port.get_next_message(subscriber_name, block)
 
     async def get_messages(
         self,
@@ -95,14 +79,7 @@ class MessageRepository:
         :param str first: Id of the first message to return.
         :param str last: Id of the last message to return.
         """
-        key = Keys.queue(subscriber_name)
-
-        response = await self.redis.xrange(key, first, last, count)
-
-        return [
-            (message_id, core.models.Message.inflate(flat_message))
-            for message_id, flat_message in response
-        ]
+        return await self.port.get_messages(subscriber_name, count, first, last)
 
     async def delete_message(self, subscriber_name: str, message_id: str):
         """Remove a message from the subscriber's queue.
@@ -110,9 +87,7 @@ class MessageRepository:
         :param str subscriber_id: Id of the subscriber.
         :param str message_id: Id of the message to delete.
         """
-
-        key = Keys.queue(subscriber_name)
-        await self.redis.xdel(key, message_id)
+        await self.port.delete_message(subscriber_name, message_id)
 
     async def delete_queue(self, subscriber_name: str):
         """Delete the entire queue for the given consumer.
@@ -120,8 +95,7 @@ class MessageRepository:
         :param str subscriber_id: Id of the subscriber.
         """
 
-        key = Keys.queue(subscriber_name)
-        await self.redis.xtrim(key, maxlen=0)
+        await self.port.delete_queue(subscriber_name)
 
 
 def get_message_repository(redis: RedisDependency) -> MessageRepository:
