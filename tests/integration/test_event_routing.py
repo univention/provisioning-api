07f1a5290cf50e64ef618a93d6352814a364d3cc
Sebastian König-Festl <sebastian.koenig.extern@univention.de>
2023-11-27 07:43:11 +0000

    test: create integration test structure

---
+++ tests/integration/test_event_routing.py
@@ -0,0 +1,5 @@
+def test_udm_create_user_event_is_routed_correctly():
+    # register a consumer
+    # call event api with new user event
+    # evaluate that the message about a new user is received by the consumer
+    pass
