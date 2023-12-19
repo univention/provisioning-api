953fd80e1e8c2caaaddd92c600133cd663aebfd1
Anton Caceres <anton@caceres.me>
2023-12-19 13:31:02 +0200

    fix: remove the _event_adapter variable from the UDMMessagingPort class

--- src/udm_messaging/port.py
+++ src/udm_messaging/port.py
@@ -12,7 +12,6 @@ from shared.models import Message
 class UDMMessagingPort:
     def __init__(self):
         self._nats_adapter = NatsAdapter()
-        self._event_adapter: Optional[EventAdapter] = None
         self._udm_adapter: Optional[UDMAdapter] = None
 
     @staticmethod
@@ -31,12 +30,12 @@ class UDMMessagingPort:
     async def store(self, url: str, new_obj: str):
         await self._nats_adapter.put_value_by_key(url, new_obj)
 
-    async def send_event(self, message: Message):
+    @staticmethod
+    async def send_event(message: Message):
         async with EventAdapter(
             settings.event_url, settings.event_username, settings.event_password
         ) as adapter:
-            self._event_adapter = adapter
-            await self._event_adapter.send_event(message)
+            await adapter.send_event(message)
 
     async def get_object(self, url: str) -> dict:
         return await self._udm_adapter.get_object(url)
