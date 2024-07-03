72110b557c556bac714041ddd21c93a09818b746
Johannes Lohmer <lohmer@univention.de>
2024-07-03 15:49:24 +0200

    fixup! fix(udm-transformer): wip add ucr to helm deployment

--- tests/integration/test_udm_messaging_service.py
+++ tests/integration/test_udm_messaging_service.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2024 Univention GmbH
+
 import pytest
 from udm_transformer.port import UDMTransformerPort
 from udm_transformer.service.udm import UDMMessagingService
