4f19602ffae5389879a06e2c431c9d15761cd580
Johannes Lohmer <lohmer@univention.de>
2024-07-10 18:06:08 +0200

    test: small udm create, delete load test

--- tests/e2e/test_event_generation.py
+++ tests/e2e/test_event_generation.py
@@ -2,15 +2,16 @@
 # SPDX-FileCopyrightText: 2024 Univention GmbH
 
 import uuid
+from pprint import pprint
 
 import pytest
 
-from univention.admin.rest.client import UDM
+from univention.admin.rest.client import UDM, BadRequest
 
 
 @pytest.fixture(scope="function")
 def maildomain(udm):
-    name = "ldif-producer.unittests"
+    name = f"ldif-producer.unittests.{str(uuid.uuid1())}"
     domains = udm.get("mail/domain")
     maildomain = domains.new()
     maildomain.properties.update({"name": name})
@@ -25,22 +26,70 @@ def test_create_delete_maildomain(maildomain):
 
 
 @pytest.mark.timeout(30)
+def test_create_user(udm: UDM, maildomain):
+    users = udm.get("users/user")
+    assert users
+
+    errors = []
+    unknown_errors = []
+
+    for i in range(200):
+        user = users.new()
+        username = str(uuid.uuid1())
+        user.properties.update(
+            {
+                "username": username,
+                "firstname": "test_event_generation",
+                "lastname": username,
+                "password": "univention",
+                "mailPrimaryAddress": f"{username}@{maildomain}",
+            }
+        )
+
+        try:
+            user.save()
+        except BadRequest as err:
+            errors.append((i, err.error_details))
+        except Exception as err:
+            unknown_errors.append((i, err))
+
+    pprint(errors)
+    pprint(unknown_errors)
+    assert not errors
+    assert not unknown_errors
+
+
 def test_create_delete_user(udm: UDM, maildomain):
     users = udm.get("users/user")
     assert users
-    user = users.new()
-    username = str(uuid.uuid1())
-    user.properties.update(
-        {
-            "username": username,
-            "firstname": "test_event_generation",
-            "lastname": username,
-            "password": "univention",
-            "mailPrimaryAddress": f"{username}@{maildomain}",
-        }
-    )
-    user.save()
-    user.delete()
+
+    errors = []
+    unknown_errors = []
+
+    for i in range(200):
+        user = users.new()
+        username = str(uuid.uuid1())
+        user.properties.update(
+            {
+                "username": username,
+                "firstname": "test_event_generation",
+                "lastname": username,
+                "password": "univention",
+                "mailPrimaryAddress": f"{username}@{maildomain}",
+            }
+        )
+        try:
+            user.save()
+            user.delete()
+        except BadRequest as err:
+            errors.append((i, err.error_details))
+        except Exception as err:
+            unknown_errors.append((i, err))
+
+    pprint(errors)
+    pprint(unknown_errors)
+    assert not errors
+    assert not unknown_errors
 
 
 # Only works after stack-data
