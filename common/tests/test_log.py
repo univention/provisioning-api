e8f961dafbc2aa4e77bf5620c3a6e80d4e7ec46a
Daniel Troeder <troeder@univention.de>
2024-10-10 13:25:16 +0200

    test(common): unit tests

--- common/tests/test_log.py
+++ common/tests/test_log.py
@@ -1,9 +1,22 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2024 Univention GmbH
 
-import pytest
+import logging
+import uuid
+
+from asgi_correlation_id import correlation_id
+
+from univention.provisioning.utils.log import setup_logging
 
 
-@pytest.mark.xfail(reason="Not implemented yet.")
-def test_setup_logging():
-    raise NotImplementedError
+def test_setup_logging(capsys):
+    """Test log format is correct and includes the request ID."""
+    req_id = uuid.uuid4().hex[:10]
+    correlation_id.set(req_id)
+    setup_logging(logging.INFO)
+    logger = logging.getLogger("foo")
+    logger.info("bar")
+    captured = capsys.readouterr()
+    assert "bar" in captured.err
+    assert "[test_log.test_setup_logging:" in captured.err
+    assert f"[{req_id}]" in captured.err
