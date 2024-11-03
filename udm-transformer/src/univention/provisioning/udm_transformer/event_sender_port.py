a4dab68f8b6e90c8741f00dbc0bc89e24bda04a2
Daniel Troeder <troeder@univention.de>
2024-11-03 10:58:37 +0100

    feat(udm-transformer): ports and adapters

--- udm-transformer/src/univention/provisioning/udm_transformer/event_sender_port.py
+++ udm-transformer/src/univention/provisioning/udm_transformer/event_sender_port.py
@@ -2,17 +2,24 @@
 # SPDX-FileCopyrightText: 2024 Univention GmbH
 
 import abc
+from typing import Self
 
 from univention.provisioning.models.message import Message
 
 
-class SendEventPort(abc.ABC):
+class EventSender(abc.ABC):
     """Send an event."""
 
     def __init__(self, url: str, username: str, password: str):
         self._url = url
         self._username = username
         self._password = password
+
+    @abc.abstractmethod
+    async def __aenter__(self) -> Self: ...
+
+    @abc.abstractmethod
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> bool: ...
 
     @abc.abstractmethod
     async def connect(self) -> None: ...
