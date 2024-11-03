c73b33d98e3c01bbe62fc99486277f87210ae7b2
Daniel Troeder <troeder@univention.de>
2024-11-03 10:58:37 +0100

    feat(udm-transformer): ports and adapters

--- udm-transformer/src/univention/provisioning/udm_transformer/main.py
+++ udm-transformer/src/univention/provisioning/udm_transformer/main.py
@@ -3,26 +3,42 @@
 
 import asyncio
 import logging
+from importlib.metadata import version
 
 from daemoniker import Daemonizer
 
+from univention.provisioning.backends.message_queue import MessageAckManager
 from univention.provisioning.utils.log import setup_logging
 
-from .config import udm_transformer_settings
-from .controller import UDMTransformerController
-from .port import UDMTransformerPort
+from .cache_adapter_nats import CacheNats
+from .config import UDMTransformerSettings, udm_transformer_settings
+from .event_sender_adapter_messages_api import MessagesRestApiEventSender
+from .ldap2udm_adapter import Ldap2UdmAdapter
+from .subscriptions_adapter_nats import NatsSubscriptions
+from .transformer_service import TransformerService
 
 UDM_TRANSFORMER_CONSUMER_NAME = "udm-transformer"
 
-
-async def run_udm_transformer() -> None:
-    async with UDMTransformerPort.port_context() as transformer_port:
-        await UDMTransformerController(transformer_port).transform_events()
+logger = logging.getLogger(__name__)
 
 
-async def main():
+async def main(settings: UDMTransformerSettings):
     with Daemonizer():
-        await run_udm_transformer()
+        async with (
+            CacheNats(settings) as cache,
+            MessagesRestApiEventSender(
+                settings.provisioning_api_url, settings.events_username_udm, settings.events_password_udm
+            ) as event_sender,
+            NatsSubscriptions(settings) as subscriptions,
+        ):
+            await TransformerService(
+                ack_manager=MessageAckManager(),
+                cache=cache,
+                event_sender=event_sender,
+                ldap2udm=Ldap2UdmAdapter(settings),
+                subscriptions=subscriptions,
+                settings=settings,
+            ).listen_for_ldap_events()
 
 
 if __name__ == "__main__":
@@ -31,4 +47,5 @@ if __name__ == "__main__":
     root_logger.handlers.clear()
     udm_transformer_settings = udm_transformer_settings()
     setup_logging(udm_transformer_settings.log_level)
-    asyncio.run(main())
+    logger.info("Starting UDM Transformer version %r.", version("nubus-provisioning-udm-transformer"))
+    asyncio.run(main(udm_transformer_settings))
