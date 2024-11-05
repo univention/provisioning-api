22a856007d07b674c7edf8eadd85bf0acbd67584
Daniel Troeder <troeder@univention.de>
2024-11-05 13:24:52 +0100

    feat(rest-api): ports and adapters

--- rest-api/src/univention/provisioning/rest/subscription_service.py
+++ rest-api/src/univention/provisioning/rest/subscription_service.py
@@ -1,18 +1,19 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2024 Univention GmbH
 
+import asyncio
 import logging
-from typing import List, Optional
+from typing import Optional
 
 import cachetools.func
 from fastapi import HTTPException, status
 from fastapi.security import HTTPBasicCredentials
 from passlib.context import CryptContext
 
-from univention.provisioning.models.constants import DISPATCHER_SUBJECT_TEMPLATE, PREFILL_SUBJECT_TEMPLATE, BucketName
 from univention.provisioning.models.subscription import FillQueueStatus, NewSubscription, Subscription
 
-from .port import Port
+from .mq_port import MessageQueuePort
+from .subscriptions_db_port import NoSubscription, SubscriptionsDBPort
 
 logger = logging.getLogger(__name__)
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -34,21 +35,25 @@ def verify_and_update_password(
 
 
 class SubscriptionService:
-    def __init__(self, port: Port):
-        self._port = port
+    def __init__(self, subscriptions_db: SubscriptionsDBPort, mq: MessageQueuePort):
+        self.sub_db = subscriptions_db
+        self.mq = mq
 
-    async def get_subscriptions(self) -> List[Subscription]:
+    async def get_subscription(self, name: str) -> Subscription:
+        """
+        Get information about a registered subscription.
+        """
+        try:
+            return await self.sub_db.load_subscription(name)
+        except NoSubscription as exc:
+            raise ValueError(str(exc))
+
+    async def get_subscriptions(self) -> list[Subscription]:
         """
         Return a list of all known subscriptions.
         """
-
-        names = await self.get_subscription_names()
-        subscriptions = [await self.get_subscription_info(name) for name in names]
-
-        return subscriptions
-
-    async def get_subscription_names(self):
-        return await self._port.get_bucket_keys(BucketName.credentials)
+        names = await self.sub_db.load_subscription_names()
+        return [await self.get_subscription(name) for name in names]
 
     @staticmethod
     def hash_password(password: str) -> str:
@@ -65,7 +70,7 @@ class SubscriptionService:
         if new_sub.realms_topics != existing_sub.realms_topics:
             return False
 
-        hashed_password = await self._port.get_str_value(new_sub.name, BucketName.credentials)
+        hashed_password = await self.sub_db.load_hashed_password(new_sub.name)
         valid = password_context.verify(new_sub.password, hashed_password)
         if not valid:
             return False
@@ -73,17 +78,22 @@ class SubscriptionService:
         return True
 
     async def register_subscription(self, new_sub: NewSubscription) -> bool:
-        """Register a new subscription."""
+        """
+        Register a new subscription.
 
-        existing_sub = await self.get_subscription_info(new_sub.name)
-        if existing_sub:
+        :returns: True is a new subscription was created or False if a matching one already exists.
+        :raises HTTPException(409): If a subscription already exists, but its configuration does not match `new_sub`.
+        """
+
+        try:
+            existing_sub = await self.sub_db.load_subscription(new_sub.name)
             if not await self.is_subscriptions_matching(new_sub, existing_sub):
                 raise HTTPException(
                     status_code=status.HTTP_409_CONFLICT,
                     detail="Subscription with the given name already registered but with different parameters.",
                 )
             return False
-        else:
+        except NoSubscription:
             logger.info(
                 "Registering new subscription (name: %r realms_topics: %r request_prefill: %r).",
                 new_sub.name,
@@ -91,7 +101,7 @@ class SubscriptionService:
                 new_sub.request_prefill,
             )
             encrypted_password = self.hash_password(new_sub.password)
-            await self._port.put_value(new_sub.name, encrypted_password, BucketName.credentials)
+            await self.sub_db.store_hashed_password(new_sub.name, encrypted_password)
             await self.prepare_and_store_subscription_info(new_sub)
             logger.info("New subscription was registered: %r", new_sub.name)
             return True
@@ -102,74 +112,34 @@ class SubscriptionService:
         else:
             prefill_queue_status = FillQueueStatus.done
 
-        sub_info = Subscription(
+        subscription = Subscription(
             name=new_sub.name,
             realms_topics=new_sub.realms_topics,
             request_prefill=new_sub.request_prefill,
             prefill_queue_status=prefill_queue_status,
         )
-        await self.set_sub_info(new_sub.name, sub_info)
-        await self._port.ensure_stream(
-            new_sub.name,
-            True,
-            [
-                DISPATCHER_SUBJECT_TEMPLATE.format(subscription=new_sub.name),
-                PREFILL_SUBJECT_TEMPLATE.format(subscription=new_sub.name),
-            ],
-        )
-        await self._port.ensure_consumer(new_sub.name)
-
-    async def get_subscription(self, name: str) -> Subscription:
-        """
-        Get information about a registered subscription.
-        """
-        if sub := await self.get_subscription_info(name):
-            return sub
-        else:
-            raise ValueError("Subscription was not found.")
-
-    async def get_subscription_info(self, name: str) -> Optional[Subscription]:
-        result = await self._port.get_dict_value(name, BucketName.subscriptions)
-        return Subscription.model_validate(result) if result else result
+        await self.sub_db.store_subscription(new_sub.name, subscription)
+        await self.mq.prepare_new_consumer_queue(new_sub.name)
+        await self.mq.create_consumer(new_sub.name)
 
     async def get_subscription_queue_status(self, name: str) -> FillQueueStatus:
         """Get the pre-fill status of the given subscription."""
-
-        sub_info = await self.get_subscription_info(name)
-        if not sub_info:
-            raise ValueError("Subscription was not found.")
-
-        return sub_info.prefill_queue_status
+        subscription = await self.get_subscription(name)
+        return subscription.prefill_queue_status
 
     async def set_subscription_queue_status(self, name: str, status: FillQueueStatus):
         """Set the pre-fill status of the given subscription."""
+        subscription = await self.get_subscription(name)
+        subscription.prefill_queue_status = status.name
+        await self.sub_db.store_subscription(name, subscription)
 
-        sub_info = await self.get_subscription_info(name)
-        if not sub_info:
-            raise ValueError("Subscription was not found.")
-
-        sub_info.prefill_queue_status = status.name
-        await self.set_sub_info(name, sub_info)
-
-    async def set_sub_info(self, name, sub_info: Subscription):
-        await self._port.put_value(name, sub_info.model_dump(), BucketName.subscriptions)
-
-    async def delete_subscription(self, name: str):
+    async def delete_subscription(self, name: str) -> None:
         """
         Delete a subscription and all of its data.
         """
-
-        sub_info = await self.get_subscription_info(name)
-        if not sub_info:
-            raise ValueError("Subscription was not found.")
-
-        await self._port.delete_kv_pair(name, BucketName.credentials)
-        await self.delete_sub_info(name)
-        await self._port.delete_stream(name)
-        await self._port.delete_consumer(name)
-
-    async def delete_sub_info(self, name: str):
-        await self._port.delete_kv_pair(name, BucketName.subscriptions)
+        _ = await self.get_subscription(name)
+        await self.sub_db.delete_subscription(name)
+        await self.mq.delete_queue(name)
 
     @staticmethod
     def handle_authentication_error(message: str):
@@ -183,15 +153,26 @@ class SubscriptionService:
         if subscription_name and subscription_name != credentials.username:
             self.handle_authentication_error("You do not have access to this data")
 
-        hashed_password = await self._port.get_str_value(credentials.username, BucketName.credentials)
+        hashed_password = await self.sub_db.load_hashed_password(credentials.username)
         cached_func_args = (credentials.password, hashed_password, subscription_name)
         valid, new_hash = verify_and_update_password(*cached_func_args)
         if valid:
             if new_hash:
                 logger.info("Storing new password hash for user %r.", credentials.username)
-                await self._port.put_value(credentials.username, new_hash, BucketName.credentials)
+                await self.sub_db.store_hashed_password(credentials.username, new_hash)
         else:
             # cache only positive authentication attempts -> remove cache entry for invalid password
             cache_key = verify_and_update_password.cache_key(*cached_func_args)
             verify_and_update_password.cache.pop(cache_key, None)
             self.handle_authentication_error("Incorrect username or password")
+
+    async def check_subscription_queue_status(self, subscription_name: str, timeout: float) -> FillQueueStatus:
+        loop = asyncio.get_event_loop()
+        end_time = loop.time() + timeout
+        while loop.time() < end_time:
+            queue_status = await self.get_subscription_queue_status(subscription_name)
+            if queue_status == FillQueueStatus.done:
+                return queue_status
+            await asyncio.sleep(1)
+
+        return await self.get_subscription_queue_status(subscription_name)
--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -8,8 +8,12 @@ import pytest
 
 from univention.provisioning.backends.mocks import MockNatsKVAdapter, MockNatsMQAdapter
 from univention.provisioning.rest.config import AppSettings
+from univention.provisioning.rest.dependencies import _kv_dependency, _mq_dependency
 from univention.provisioning.rest.main import app
-from univention.provisioning.rest.port import Port
+from univention.provisioning.rest.mq_adapter_nats import NatsMessageQueue
+from univention.provisioning.rest.mq_port import MessageQueuePort
+from univention.provisioning.rest.subscriptions_db_adapter_nats import NatsSubscriptionsDB
+from univention.provisioning.rest.subscriptions_db_port import SubscriptionsDBPort
 from univention.provisioning.testing.mock_data import NATS_SERVER
 
 pytest_plugins = ["univention.provisioning.testing.conftest"]
@@ -48,18 +52,30 @@ def mock_nats_mq_adapter(nats_credentials) -> MockNatsMQAdapter:
 
 
 @pytest.fixture
-async def port_fake_dependency(mock_nats_kv_adapter, mock_nats_mq_adapter) -> Callable[[], Port]:
-    port = Port(AppSettings(nats_user="api", nats_password="apipass"))
-    port.mq_adapter = mock_nats_mq_adapter
-    port.kv_adapter = mock_nats_kv_adapter
-    return lambda: port
+def fake_app_settings() -> AppSettings:
+    return AppSettings(nats_user="api", nats_password="apipass")
+
+
+@pytest.fixture
+async def kv_fake_dependency(fake_app_settings, mock_nats_kv_adapter) -> Callable[[], SubscriptionsDBPort]:
+    kv = NatsSubscriptionsDB(fake_app_settings)
+    kv.kv = mock_nats_kv_adapter
+    return lambda: kv
+
+
+@pytest.fixture
+async def mq_fake_dependency(fake_app_settings, mock_nats_mq_adapter) -> Callable[[], MessageQueuePort]:
+    mq = NatsMessageQueue(fake_app_settings)
+    mq.mq = mock_nats_mq_adapter
+    return lambda: mq
 
 
 @pytest.fixture(autouse=True)
-def override_dependencies(port_fake_dependency):
+def override_dependencies(kv_fake_dependency, mq_fake_dependency):
     # Override original port
     # app.dependency_overrides[AppSettingsDep] = settings_mock
-    app.dependency_overrides[Port.port_dependency] = port_fake_dependency
+    app.dependency_overrides[_kv_dependency] = kv_fake_dependency
+    app.dependency_overrides[_mq_dependency] = mq_fake_dependency
     yield  # This will ensure the setup is done before tests and cleanup after
     # Clear the overrides after the tests
     app.dependency_overrides.clear()
