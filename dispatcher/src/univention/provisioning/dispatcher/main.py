bdfa78dea4f57166f22b5506fcab2a215f398a26
Daniel Troeder <troeder@univention.de>
2024-11-05 10:07:01 +0100

    feat(dispatcher): ports and adapters

--- dispatcher/src/univention/provisioning/dispatcher/main.py
+++ dispatcher/src/univention/provisioning/dispatcher/main.py
@@ -2,25 +2,28 @@
 # SPDX-FileCopyrightText: 2024 Univention GmbH
 
 import asyncio
+import logging
+from importlib.metadata import version
 
+from univention.provisioning.backends.message_queue import MessageAckManager
 from univention.provisioning.utils.log import setup_logging
 
-from .config import dispatcher_settings
-from .port import DispatcherPort
+from .config import DispatcherSettings, dispatcher_settings
+from .mq_adapter_nats import NatsMessageQueueAdapter
 from .service import DispatcherService
+from .subscriptions_adapter_nats import NatsSubscriptionsAdapter
+
+logger = logging.getLogger(__name__)
 
 
-async def run_dispatcher():
-    async with DispatcherPort.port_context() as port:
-        service = DispatcherService(port)
+async def main(settings: DispatcherSettings):
+    async with NatsMessageQueueAdapter(settings) as mq, NatsSubscriptionsAdapter(settings) as subscriptions:
+        service = DispatcherService(ack_manager=MessageAckManager(), mq=mq, subscriptions=subscriptions)
         await service.dispatch_events()
-
-
-def main():
-    asyncio.run(run_dispatcher())
 
 
 if __name__ == "__main__":
     dispatcher_settings = dispatcher_settings()
     setup_logging(dispatcher_settings.log_level)
-    main()
+    logger.info("Starting Dispatcher service version %r.", version("nubus-provisioning-dispatcher"))
+    asyncio.run(main(dispatcher_settings))
--- tests/integration/test_dispatcher_service.py
+++ tests/integration/test_dispatcher_service.py
@@ -6,28 +6,40 @@ from unittest.mock import AsyncMock, call
 
 import pytest
 
-from univention.provisioning.backends.mocks import MockNatsKVAdapter, MockNatsMQAdapter
 from univention.provisioning.backends.nats_mq import json_decoder
 from univention.provisioning.dispatcher.config import DispatcherSettings
-from univention.provisioning.dispatcher.port import DispatcherPort
-from univention.provisioning.dispatcher.service import DispatcherService
+from univention.provisioning.dispatcher.service import DispatcherService, MessageAckManager
 from univention.provisioning.models.constants import DISPATCHER_SUBJECT_TEMPLATE
 from univention.provisioning.testing.mock_data import FLAT_MESSAGE_ENCODED, MSG, SUBSCRIPTION_NAME, SUBSCRIPTIONS
 
 
 @pytest.fixture
-async def dispatcher_mock(mock_nats_kv_adapter, mock_nats_mq_adapter) -> DispatcherPort:
-    port = DispatcherPort(DispatcherSettings(nats_user="dispatcher", nats_password="dispatcherpass"))
-    port.mq = MockNatsMQAdapter()
-    port.kv = MockNatsKVAdapter()
+def dispatcher_service(mock_nats_kv_adapter, mock_nats_mq_adapter) -> DispatcherService:
+    settings = DispatcherSettings(nats_user="dispatcher", nats_password="dispatcherpass")
+    mq = NatsMessageQueueAdapter(settings)
+    mq.mq = mock_nats_mq_adapter
+    subs = NatsSubscriptionsAdapter(settings)
+    subs.kv = mock_nats_kv_adapter
     fake_ack = AsyncMock()
     mod_MSG = deepcopy(MSG)
     mod_MSG.data = json_decoder(mod_MSG.data.decode())
-    port.mq.get_one_message = AsyncMock(
+    mq.get_one_message = AsyncMock(
         side_effect=[(mod_MSG, fake_ack), StopLoopException("Stop waiting for the new event")]
     )
+    return DispatcherService(ack_manager=MessageAckManager(), mq=mq, subscriptions=subs)
 
-    return port
+
+# @pytest.fixture
+# async def dispatcher_mock(mock_nats_kv_adapter, mock_nats_mq_adapter) -> DispatcherPort:
+#     port = DispatcherPort(DispatcherSettings(nats_user="dispatcher", nats_password="dispatcherpass"))
+#     port.mq = mock_nats_mq_adapter
+#     port.kv = mock_nats_kv_adapter
+#     port.mq._message_queue.get = AsyncMock(side_effect=[MSG, Exception("Stop waiting for the new event")])
+#     port.watch_for_subscription_changes = AsyncMock()
+#     Msg.in_progress = AsyncMock()
+#     Msg.ack = AsyncMock()
+#
+#     return port
 
 
 class StopLoopException(Exception): ...
@@ -37,23 +49,22 @@ class StopLoopException(Exception): ...
 class TestDispatcher:
     main_subject = DISPATCHER_SUBJECT_TEMPLATE.format(subscription=SUBSCRIPTION_NAME)
 
-    async def test_dispatch_events(self, dispatcher_mock: DispatcherPort):
+    async def test_dispatch_events(self, dispatcher_service: DispatcherService):
         """
         This abstract test focuses on checking the interaction between the Dispatcher Service and the Nats,
         specifically verifying the usage of the NATS and jetstream. If the technology changes, the test will fail
         """
 
         # trigger dispatcher to retrieve event from incoming queue
-        service = DispatcherService(dispatcher_mock)
-        service._subscriptions = SUBSCRIPTIONS
+        dispatcher_service._subscriptions = SUBSCRIPTIONS
 
         with pytest.raises(ExceptionGroup):
-            await service.dispatch_events()
+            await dispatcher_service.dispatch_events()
 
         # check subscribing to the incoming queue
-        dispatcher_mock.mq._js.subscribe.assert_called_once_with(
+        dispatcher_service.mq.mq._js.subscribe.assert_called_once_with(
             "incoming",
-            cb=dispatcher_mock.mq.cb,
+            cb=dispatcher_service.mq.mq.cb,
             durable="durable_name:incoming",
             stream="stream:incoming",
             manual_ack=True,
@@ -62,10 +73,12 @@ class TestDispatcher:
         dispatcher_mock.mq.get_one_message.assert_has_calls([call(timeout=10), call(timeout=10)])
 
         # check getting subscriptions for the realm_topic
-        dispatcher_mock.watch_for_subscription_changes.assert_called_once_with(service.update_subscriptions_mapping)
+        dispatcher_service.subscriptions_db.watch_for_subscription_changes.assert_called_once_with(
+            dispatcher_service.update_subscriptions_mapping
+        )
 
         # check storing event in the consumer queue
-        dispatcher_mock.mq._js.publish.assert_called_once_with(
+        dispatcher_service.mq.mq._js.publish.assert_called_once_with(
             self.main_subject,
             FLAT_MESSAGE_ENCODED,
             stream=f"stream:{SUBSCRIPTION_NAME}",
