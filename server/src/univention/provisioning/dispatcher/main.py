15f9373c3c874eabf55c64d200b9151169500233
Johannes Lohmer <lohmer@univention.de>
2025-02-01 23:28:59 +0100

    fix(server): Remove problematic version logging and make the rest-api more easily executable

--- server/src/univention/provisioning/dispatcher/main.py
+++ server/src/univention/provisioning/dispatcher/main.py
@@ -3,7 +3,6 @@
 
 import asyncio
 import logging
-from importlib.metadata import version
 
 from univention.provisioning.backends.message_queue import MessageAckManager
 from univention.provisioning.utils.log import setup_logging
@@ -25,7 +24,6 @@ async def main(settings: DispatcherSettings):
 def run():
     settings = dispatcher_settings()
     setup_logging(settings.log_level)
-    logger.info("Starting Dispatcher service version %r.", version("nubus-provisioning-dispatcher"))
     asyncio.run(main(settings))
 
 
