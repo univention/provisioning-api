874ad1c45b40ba0906e583b59d77f4b61d9d0974
Johannes Lohmer <lohmer@univention.de>
2025-02-02 13:27:23 +0100

    fix(e2e-tests): Last e2e fixes to get a clean e2e test run after the project restructuring

--- e2e_tests/tests/test_consumer_client_callback.py
+++ e2e_tests/tests/test_consumer_client_callback.py
@@ -3,13 +3,19 @@
 
 import pytest
 
-from univention.provisioning.consumer.api import MessageHandler, ProvisioningConsumerClient
+from univention.provisioning.consumer.api import MessageHandler, MessageHandlerSettings, ProvisioningConsumerClient
 from univention.provisioning.models.message import Message
 
 
+@pytest.fixture()
 async def test_no_callback_function_provided(provisioning_client: ProvisioningConsumerClient, dummy_subscription: str):
     with pytest.raises(ValueError, match="Callback functions can't be empty"):
-        await MessageHandler(provisioning_client, [], message_limit=1).run()
+        await MessageHandler(
+            client=provisioning_client,
+            callbacks=[],
+            settings=MessageHandlerSettings(max_acknowledgement_retries=5),
+            message_limit=1,
+        ).run()
 
 
 @pytest.mark.timeout(10)
@@ -26,7 +32,12 @@ async def test_get_one_message(
     async def test_callback(message: Message):
         result.append(message)
 
-    await MessageHandler(provisioning_client, [test_callback], message_limit=1).run()
+    await MessageHandler(
+        client=provisioning_client,
+        callbacks=[test_callback],
+        settings=MessageHandlerSettings(max_acknowledgement_retries=5),
+        message_limit=1,
+    ).run()
 
     assert len(result) == 1
 
@@ -63,6 +74,11 @@ async def test_get_multiple_messages(
     async def test_callback(message: Message):
         result.append(message)
 
-    await MessageHandler(provisioning_client, [test_callback], message_limit=3).run()
+    await MessageHandler(
+        client=provisioning_client,
+        callbacks=[test_callback],
+        settings=MessageHandlerSettings(max_acknowledgement_retries=5),
+        message_limit=3,
+    ).run()
 
     assert len(result) == 3
