802cc24f8ed90b4f58f59bc5cbf597bd26abd49c
Johannes Lohmer <lohmer@univention.de>
2025-04-24 08:49:54 +0200

    refactor(example-consumer): refactor example consumer
    univention/dev/internal/team-nubus#1038

--- consumer_example/src/univention/provisioning/consumer_example/main.py
+++ consumer_example/src/univention/provisioning/consumer_example/main.py
@@ -53,28 +53,32 @@ def parse_args(argv: Sequence[str]) -> argparse.Namespace:
     return arguments
 
 
+async def _create_subscription(arguments, base_url):
+    admin_settings = ProvisioningConsumerClientSettings(
+        provisioning_api_username=arguments.admin_username,
+        provisioning_api_password=arguments.admin_password,
+        provisioning_api_base_url=base_url,
+    )
+    realms_topics_args = [realm_topic.split(":") for realm_topic in arguments.realm_topic]
+    assert all(len(x) == 2 for x in realms_topics_args), "'realm_topic' argument must contain exactly one colon."
+    prefill = arguments.prefill
+    async with ProvisioningConsumerClient(admin_settings) as admin_client:
+        try:
+            await admin_client.create_subscription(
+                admin_settings.provisioning_api_username,
+                admin_settings.provisioning_api_password,
+                [RealmTopic(realm=realm, topic=topic) for realm, topic in realms_topics_args],
+                prefill,
+            )
+        except ClientResponseError as e:
+            logger.warning("%s, Client already exists", e)
+
+
 async def main(settings: ProvisioningConsumerClientSettings) -> None:
     arguments = parse_args(sys.argv[1:])
 
     if len(sys.argv) > 1:
-        admin_settings = ProvisioningConsumerClientSettings(
-            provisioning_api_username=arguments.admin_username,
-            provisioning_api_password=arguments.admin_password,
-            provisioning_api_base_url=settings.provisioning_api_base_url,
-        )
-        realms_topics_args = [realm_topic.split(":") for realm_topic in arguments.realm_topic]
-        assert all(len(x) == 2 for x in realms_topics_args), "'realm_topic' argument must contain exactly one colon."
-        prefill = arguments.prefill
-        async with ProvisioningConsumerClient(admin_settings) as admin_client:
-            try:
-                await admin_client.create_subscription(
-                    settings.provisioning_api_username,
-                    settings.provisioning_api_password,
-                    [RealmTopic(realm=realm, topic=topic) for realm, topic in realms_topics_args],
-                    prefill,
-                )
-            except ClientResponseError as e:
-                logger.warning("%s, Client already exists", e)
+        await _create_subscription(arguments, settings.provisioning_api_base_url)
 
     logger.info("Listening for messages")
     async with ProvisioningConsumerClient(settings) as client:
@@ -82,11 +86,11 @@ async def main(settings: ProvisioningConsumerClientSettings) -> None:
 
 
 def run():
-    _settings = ProvisioningConsumerClientSettings()
-    logging.basicConfig(format=LOG_FORMAT, level=_settings.log_level)
+    settings = ProvisioningConsumerClientSettings()
+    logging.basicConfig(format=LOG_FORMAT, level=settings.log_level)
     logger.info("args: %r", sys.argv)
     logger.info("Using 'nubus-provisioning-consumer' library version %r.", version("nubus-provisioning-consumer"))
-    asyncio.run(main(_settings))
+    asyncio.run(main(settings))
 
 
 if __name__ == "__main__":
