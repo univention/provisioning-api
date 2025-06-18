e0987e85a4031a8a1d9b65cf5d8cde9b8a30c71e
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-18 21:17:52 +0200

    feat: Common secret handling for udmTransformer.nats.auth

---
+++ tests/chart/provisioning/test_udm_transformer_nats.py
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+from univention.testing.helm.client.nats import (
+    AuthPassword,
+    SecretUsageViaEnv,
+)
+
+
+class TestAuthPassword(SecretUsageViaEnv, AuthPassword):
+    workload_name = "release-name-provisioning-udm-transformer"
+    secret_name = "release-name-provisioning-udm-transformer-nats"
+
+    prefix_mapping = {
+        "udmTransformer.nats": "nats",
+    }
+
+
+class TestWaitForNatsAuthPassword(TestAuthPassword):
+
+    path_container = "spec.template.spec.initContainers[?@.name=='wait-for-nats']"
