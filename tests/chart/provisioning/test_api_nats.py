2e7c9b5bd5319db7bf9504561ccef101f6ec7dc4
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-18 20:50:47 +0200

    feat(provisioning): Common secret handling for "api.nats.auth"
    Issue: univention/dev/internal/team-nubus#892

---
+++ tests/chart/provisioning/test_api_nats.py
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+from univention.testing.helm.client.nats import (
+    AuthPassword,
+    SecretUsageViaEnv,
+)
+
+
+class TestAuthPassword(SecretUsageViaEnv, AuthPassword):
+    workload_name = "release-name-provisioning-api"
+    secret_name = "release-name-provisioning-api-nats"
+
+    path_main_container = "spec.template.spec.containers[?@.name=='api']"
+
+    prefix_mapping = {
+        "api.nats": "nats",
+    }
+
+
+class TestWaitForNatsAuthPassword(TestAuthPassword):
+
+    path_main_container = "spec.template.spec.initContainers[?@.name=='wait-for-nats']"
