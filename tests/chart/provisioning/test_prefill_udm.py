6cbec3df0293bae766071be77a8f2a5bd28b1baa
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-19 09:00:25 +0200

    feat: Common secret handling for prefill.udm.auth
    Issue: univention/dev/internal/team-nubus#892

---
+++ tests/chart/provisioning/test_prefill_udm.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+from univention.testing.helm.client.udm import AuthPassword, SecretViaEnv
+
+
+class TestAuth(SecretViaEnv, AuthPassword):
+    config_map_name = "release-name-provisioning-prefill"
+    secret_name = "release-name-provisioning-prefill-udm"
+    workload_name = "release-name-provisioning-prefill"
+
+    prefix_mapping = {
+        "prefill.udm": "udm",
+    }
+
+class TestAuthWaitForUdm(TestAuth):
+
+    path_container = "spec.template.spec.initContainers[?@.name=='wait-for-udm']"
