e41ff5b8cdf2c5190c979d0a3829d9282b9e2703
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-19 22:05:00 +0200

    feat(udm-listener): Use common secret behavior around ldap.auth

---
+++ tests/chart/udm-listener/test_ldap.py
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+from univention.testing.helm.client.ldap import Auth, SecretViaVolume
+
+
+class TestLdapClient(SecretViaVolume, Auth):
+
+    config_map_name = "release-name-udm-listener"
+    secret_name = "release-name-udm-listener-ldap"
+    workload_kind = "StatefulSet"
+    workload_name = "release-name-udm-listener"
+
+    path_ldap_bind_dn = "data.LDAP_HOST_DN"
