dd158d800df4acc3a73e6a96ab3595f33b1d141f
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-19 07:19:00 +0200

    feat: Common secrets handling for prefill.nats.auth
    Issue: univention/dev/internal/team-nubus#892

---
+++ tests/chart/provisioning/test_prefill_nats.py
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+from univention.testing.helm.client.nats import (
+    AuthPassword,
+    SecretUsageViaEnv,
+)
+
+
+class TestAuthPassword(SecretUsageViaEnv, AuthPassword):
+    workload_name = "release-name-provisioning-prefill"
+    secret_name = "release-name-provisioning-prefill-nats"
+
+    prefix_mapping = {
+        "prefill.nats": "nats",
+    }
