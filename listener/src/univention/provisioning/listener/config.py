43b859f51a7dc4985dd04d07f326325e3a58c195
Iván Barbecho Delgado <ivan.delgado@univention.de>
2025-06-19 15:06:36 +0200

    feat(listener): univention/dev/ucs#2902 debian packaging of listener module

--- listener/src/univention/provisioning/listener/config.py
+++ listener/src/univention/provisioning/listener/config.py
@@ -1,35 +1,9 @@
-# SPDX-License-Identifier: AGPL-3.0-only
-# SPDX-FileCopyrightText: 2024 Univention GmbH
+import os
 
-from functools import lru_cache
+if os.getenv("READ_CONFIG_FROM_ENVIRONMENT_VARIABLES", "false").lower() == "true":
+    from .config_env import LdapProducerSettings, ldap_producer_settings
+else:
+    from .config_ucr import LdapProducerSettings, ldap_producer_settings
 
-from pydantic import conint
-from pydantic_settings import BaseSettings
-
-
-class LdapProducerSettings(BaseSettings):
-    # Nats user name specific to UdmProducerSettings
-    nats_user: str
-    # Nats password specific to UdmProducerSettings
-    nats_password: str
-    # Nats: host
-    nats_host: str
-    # Nats: port
-    nats_port: int
-    # Maximum number of reconnect attempts to the NATS server
-    nats_max_reconnect_attempts: int
-    # Delay between retry attempts to the NATS server (in seconds)
-    nats_retry_delay: conint(ge=0)
-    # Maximum number of retry attempts for interacting with the NATS server
-    nats_max_retry_count: conint(ge=0)
-    # Enable termination on NATS failure
-    terminate_listener_on_exception: bool
-
-    @property
-    def nats_server(self) -> str:
-        return f"nats://{self.nats_host}:{self.nats_port}"
-
-
-@lru_cache(maxsize=1)
-def ldap_producer_settings() -> LdapProducerSettings:
-    return LdapProducerSettings()
+LdapProducerSettings = LdapProducerSettings
+ldap_producer_settings = ldap_producer_settings
