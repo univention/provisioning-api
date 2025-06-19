aedeb1abca11c7055302574faf3e35e50a63322e
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-19 10:59:40 +0200

    feat: Support owner role for "api.auth.admin"

--- tests/chart/provisioning/test_api_auth_admin.py
+++ tests/chart/provisioning/test_api_auth_admin.py
@@ -1,16 +1,17 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-from univention.testing.helm.client.provisioning_api import AuthPassword, SecretViaEnv
+from univention.testing.helm.client.provisioning_api import AuthPassword, AuthPasswordOwner, SecretViaEnv
 
 
-class TestAuth(SecretViaEnv, AuthPassword):
+class TestAuth(SecretViaEnv, AuthPasswordOwner, AuthPassword):
 
-    config_map_name = "release-name-provisioning-api"
     secret_name = "release-name-provisioning-api-admin"
     workload_name = "release-name-provisioning-api"
 
     sub_path_env_password = "env[?@name=='ADMIN_PASSWORD']"
+
+    derived_password = "751120bf3b933a18b7d637bfba5e9389939c4bbd"
 
     prefix_mapping = {
         "api.auth.admin": "provisioningApi.auth",
