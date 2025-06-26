eb659b26dcdafa700bc9747588aacd9cebd4d802
Iván Barbecho Delgado <ivan.delgado@univention.de>
2025-06-26 12:43:24 +0200

    fix(listener): univention/dev/ucs#2902 control traceback during the connection to the nats server.

--- listener/src/univention/provisioning/listener/config.py
+++ listener/src/univention/provisioning/listener/config.py
@@ -27,6 +27,7 @@ def ucr_ldap_producer_settings(settings: BaseSettings) -> Dict[str, Any]:
         "nats_max_reconnect_attempts": ucr.get("nats/max_reconnect_attempts", 10),
         "nats_retry_delay": ucr.get("nats/retry_delay", 10),
         "nats_max_retry_count": ucr.get("nats/max_retry_count", 3),
+        "nats_password": "univention",
     }
 
     nats_password_file = ucr.get("nats/passwordfile", "/etc/nats/provisioning-listener.secret")
