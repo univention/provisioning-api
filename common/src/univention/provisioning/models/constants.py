0961b2971dd359a4bfb86c3f45f6440ad461a2e3
Johannes Lohmer <lohmer@univention.de>
2025-07-06 13:46:20 +0200

    fixup! fix(listener): Make the listener code independent of the common models because they depend on pydantic v2

---
+++ common/src/univention/provisioning/models/constants.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2024 Univention GmbH
+
+from enum import Enum
+
+LDIF_PRODUCER_QUEUE_NAME = "ldif-producer"
+LDAP_PRODUCER_QUEUE_NAME = "ldap-producer"
+PREFILL_QUEUE_NAME = "prefill"
+DISPATCHER_SUBJECT_TEMPLATE = "{subscription}.main"
+DISPATCHER_QUEUE_NAME = "incoming"
+PREFILL_SUBJECT_TEMPLATE = "{subscription}.prefill"
+
+
+class BucketName(str, Enum):
+    subscriptions = "SUBSCRIPTIONS"
+    credentials = "CREDENTIALS"
+    cache = "CACHE"
+
+
+class PublisherName(str, Enum):
+    udm_listener = "udm-listener"
+    ldif_producer = "ldif-producer"
+    udm_pre_fill = "udm-pre-fill"
+    consumer_registration = "consumer-registration"
+    consumer_client_test = "consumer_client_test"
