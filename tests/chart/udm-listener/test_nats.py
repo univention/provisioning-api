37b40018b606fe488092d0ceb9bfa31dc3aee0b1
Johannes Lohmer <lohmer@univention.de>
2025-09-24 23:09:35 +0200

    fix(nats): Integrate improved nats user configuration and migrate helm unittests to flavor-based helm test harness
    univention/dev/internal/team-nubus#1399

--- tests/chart/udm-listener/test_nats.py
+++ tests/chart/udm-listener/test_nats.py
@@ -1,13 +1,17 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-from univention.testing.helm.client.nats import (
-    AuthPassword,
-    SecretUsageViaEnv,
-)
+from univention.testing.helm.auth_flavors.password_usage import AuthPasswordUsageViaEnv
+from univention.testing.helm.auth_flavors.secret_generation import AuthSecretGenerationOwner
 
 
-class TestAuthPassword(SecretUsageViaEnv, AuthPassword):
+class TestAuth(AuthSecretGenerationOwner, AuthPasswordUsageViaEnv):
     workload_kind = "StatefulSet"
     workload_name = "release-name-udm-listener"
     secret_name = "release-name-udm-listener-nats"
+
+    prefix_mapping = {
+        "nats.auth": "auth",
+    }
+    sub_path_env_password = "env[?@name=='NATS_PASSWORD']"
+    derived_password = "c4bcaffc26984205d02265dd6c1894d7ad6e9d45"
