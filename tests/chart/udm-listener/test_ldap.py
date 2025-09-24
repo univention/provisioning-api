0801b8539e41e1e6ea253b18b5b82263d4357fcd
Johannes Lohmer <lohmer@univention.de>
2025-09-24 23:09:35 +0200

    fix(nats): Integrate improved nats user configuration and migrate helm unittests to flavor-based helm test harness
    univention/dev/internal/team-nubus#1399

--- tests/chart/udm-listener/test_ldap.py
+++ tests/chart/udm-listener/test_ldap.py
@@ -1,13 +1,18 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-from univention.testing.helm.client.ldap import Auth, SecretViaVolume
+from univention.testing.helm.auth_flavors.password_usage import AuthPasswordUsageViaVolume
+from univention.testing.helm.auth_flavors.secret_generation import AuthSecretGenerationUser
 
 
-class TestLdapClient(SecretViaVolume, Auth):
+class TestAuth(AuthSecretGenerationUser, AuthPasswordUsageViaVolume):
     config_map_name = "release-name-udm-listener"
     secret_name = "release-name-udm-listener-ldap"
     workload_kind = "StatefulSet"
     workload_name = "release-name-udm-listener"
 
-    path_ldap_bind_dn = "data.LDAP_HOST_DN"
+    prefix_mapping = {
+        "ldap.auth": "auth",
+    }
+
+    volume_name = "secret-ldap"
