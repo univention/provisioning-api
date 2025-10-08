8a0a316c6cf5101f637ab9b51f22b3ffa5a67ef0
Christian Castens <castens@univention.de>
2025-10-08 09:12:20 +0200

    feat(app): add run-backup-* scripts

---
+++ run-backup-consumer.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+docker system prune -f
+export PROVISIONING_API_ADMIN_PASSWORD=$(jq -r '.PROVISIONING_API_ADMIN_PASSWORD' /etc/provisioning-json.secrets)
+export PROVISIONING_API_ADMIN_USER=admin
+export PROVISIONING_API_URL=https://master.ucs.test/univention/provisioning/
+export NATS_USER=api
+export NATS_PASSWORD=$(jq -r '.NATS_PASSWORD' /etc/provisioning-json.secrets)
+export TARGET_NATS=nats://nats:4222
+export PROVISIONING_API_BASE_URL="http://provisioning-api:7777"
+
+mkdir -p secrets
+
+echo "
+services:
+  test:
+    image: harbor-registry.atlantishq.de/atlantishq/backup-consumer:latest
+    container_name: backup-consumer-test
+    command: >
+      backup_consumer
+      --provisioning-api-admin-user "${PROVISIONING_API_ADMIN_USER}"
+      --provisioning-api-admin-user-password "${PROVISIONING_API_ADMIN_PASSWORD}"
+      --nats-user "${NATS_USER}"
+      --nats-password "${NATS_PASSWORD}"
+      --target-nats "${TARGET_NATS}"
+      --provisioning-url ${PROVISIONING_API_URL}
+      --stream-name "backup-4"
+    environment:
+      PROVISIONING_API_USERNAME: ${PROVISIONING_API_ADMIN_USERNAME}
+      PROVISIONING_API_PASSWORD: ${PROVISIONING_API_ADMIN_PASSWORD}
+      LOG_LEVEL: DEBUG
+      PROVISIONING_API_BASE_URL: ${PROVISIONING_API_URL}
+      MAX_ACKNOWLEDGEMENT_RETRIES: 5
+    volumes:
+      - /etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt:ro
+    networks:
+      nubus-provisioning:
+networks:
+  nubus-provisioning:
+    external: true
+" > client.yaml
+
+docker-compose -f client.yaml pull
+docker-compose -f client.yaml up
