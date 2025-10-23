c90d2d5f659fec9c1844ea8698ce3afffe8b8a6b
Iván Barbecho Delgado <ivan.delgado@univention.de>
2025-10-23 16:41:39 +0200

    fix: env_suffix does not work in pydantic_BaseSettings
    Issue: univention/dev/projects/provisioning#96

--- dispatcher/src/univention/provisioning/dispatcher/config.py
+++ dispatcher/src/univention/provisioning/dispatcher/config.py
@@ -9,9 +9,14 @@ from pydantic_settings import BaseSettings
 Loglevel = Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 
 
-class DispatcherSettings(BaseSettings):
+class BaseDispatcherSettings(BaseSettings):
     # Python log level
     log_level: Loglevel
+    # Maximum number of reconnect attempts to the NATS server
+    nats_max_reconnect_attempts: int = 2
+
+
+class DispatcherSettings(BaseDispatcherSettings):
 
     # Nats user name specific to Dispatcher
     nats_user: str
@@ -21,24 +26,80 @@ class DispatcherSettings(BaseSettings):
     nats_host: str
     # Nats: port
     nats_port: int
-    # Maximum number of reconnect attempts to the NATS server
-    nats_max_reconnect_attempts: int = 2
 
     @property
     def nats_server(self) -> str:
         return f"nats://{self.nats_host}:{self.nats_port}"
 
 
-class DispatcherSettingsPull(DispatcherSettings):
-    model_config = ConfigDict(env_prefix="", env_suffix="_PULL")
+class DispatcherSettingsPull(BaseDispatcherSettings):
+
+    # Nats user name specific to Dispatcher
+    nats_user_pull: str
+    # Nats password specific to Dispatcher
+    nats_password_pull: str
+    # Nats: host
+    nats_host_pull: str
+    # Nats: port
+    nats_port_pull: int
+    # Maximum number of reconnect attempts to the NATS server
+    nats_max_reconnect_attempts: int = 2
+
+    @property
+    def nats_server(self) -> str:
+        return f"nats://{self.nats_host_pull}:{self.nats_port_pull}"
+
+    @property
+    def nats_user(self) -> str:
+        return self.nats_user_pull
+
+    @property
+    def nats_password(self) -> str:
+        return self.nats_password_pull
+
+    @property
+    def nats_host(self) -> str:
+        return self.nats_host_pull
+
+    @property
+    def nats_port(self) -> int:
+        return self.nats_port_pull
 
 
-class DispatcherSettingsPush(DispatcherSettings):
-    model_config = ConfigDict(env_prefix="", env_suffix="_PUSH")
+class DispatcherSettingsPush(BaseDispatcherSettings):
+
+    # Nats user name specific to Dispatcher
+    nats_user_push: str
+    # Nats password specific to Dispatcher
+    nats_password_push: str
+    # Nats: host
+    nats_host_push: str
+    # Nats: port
+    nats_port_push: int
+
+    @property
+    def nats_server(self) -> str:
+        return f"nats://{self.nats_host_push}:{self.nats_port_push}"
+
+    @property
+    def nats_user(self) -> str:
+        return self.nats_user_push
+
+    @property
+    def nats_password(self) -> str:
+        return self.nats_password_push
+
+    @property
+    def nats_host(self) -> str:
+        return self.nats_host_push
+
+    @property
+    def nats_port(self) -> int:
+        return self.nats_port_push
 
 
 @lru_cache(maxsize=1)
-def dispatcher_settings_pull() -> DispatcherSettings:
+def dispatcher_settings_pull() -> BaseDispatcherSettings:
     try:
         return DispatcherSettingsPull()
     except ValidationError:
@@ -46,12 +107,13 @@ def dispatcher_settings_pull() -> DispatcherSettings:
 
 
 @lru_cache(maxsize=1)
-def dispatcher_settings_push() -> DispatcherSettings:
+def dispatcher_settings_push() -> BaseDispatcherSettings:
     try:
         return DispatcherSettingsPush()
     except ValidationError:
         return DispatcherSettings()
 
+
 @lru_cache(maxsize=1)
-def dispatcher_settings() -> DispatcherSettings:
+def dispatcher_settings() -> BaseDispatcherSettings:
     return DispatcherSettings()
