0c1bd94d58248a09c517d197b1aec8ff5715bb15
Iván Barbecho Delgado <ivan.delgado@univention.de>
2025-10-31 10:55:30 +0100

    test: fix e2e test
    Issue: univention/dev/projects/provisioning#95

--- e2e_tests/tests/test_nats_message_queue_adapter.py
+++ e2e_tests/tests/test_nats_message_queue_adapter.py
@@ -4,6 +4,7 @@
 import uuid
 
 import pytest
+from nats.js.api import RetentionPolicy
 from nats.js.errors import BadRequestError, ServerError
 
 from univention.provisioning.backends.nats_mq import ConsumerQueue, NatsMessageQueue
@@ -20,60 +21,65 @@ async def nats_mq_adapter(test_settings: E2ETestSettings):
 
 
 @pytest.fixture
-async def test_stream(nats_mq_adapter: NatsMessageQueue):
+async def test_queue(nats_mq_adapter: NatsMessageQueue):
     stream_name = "just_testing_again"
-    await nats_mq_adapter.ensure_stream(stream_name, False)
-    yield stream_name
-    await nats_mq_adapter.delete_stream(stream_name)
+    queue = ConsumerQueue(stream_name)
+    queue.retention_policy = RetentionPolicy.WORK_QUEUE
+    await nats_mq_adapter.ensure_stream(queue)
+    yield queue
+    await nats_mq_adapter.delete_stream(queue)
 
 
 @pytest.fixture
-async def test_consumer(nats_mq_adapter: NatsMessageQueue, test_stream: str):
-    await nats_mq_adapter.ensure_consumer(test_stream)
+async def test_consumer(nats_mq_adapter: NatsMessageQueue, test_queue: ConsumerQueue):
+    await nats_mq_adapter.ensure_consumer(test_queue)
     yield True
-    await nats_mq_adapter.delete_consumer(test_stream)
+    await nats_mq_adapter.delete_consumer(test_queue)
 
 
 async def test_nats_connection(nats_mq_adapter: NatsMessageQueue):
     assert nats_mq_adapter
 
 
-async def test_ensure_stream(nats_mq_adapter: NatsMessageQueue, test_stream):
-    await nats_mq_adapter.ensure_stream(test_stream, False)
-    await nats_mq_adapter.ensure_stream(test_stream, False)
-    await nats_mq_adapter.ensure_stream(test_stream, False)
+async def test_ensure_stream(nats_mq_adapter: NatsMessageQueue, test_queue: ConsumerQueue):
+    await nats_mq_adapter.ensure_stream(test_queue)
+    await nats_mq_adapter.ensure_stream(test_queue)
+    await nats_mq_adapter.ensure_stream(test_queue)
 
 
 async def test_ensure_stream_with_filter(nats_mq_adapter: NatsMessageQueue):
-    stream_name = "just_testing"
-    await nats_mq_adapter.ensure_stream(stream_name, False, ["just_testing"])
-    await nats_mq_adapter.delete_stream(stream_name)
+    queue = ConsumerQueue("just_testing")
+    await nats_mq_adapter.ensure_stream(queue)
+    await nats_mq_adapter.delete_stream(queue)
 
 
 async def test_ensure_stream_with_conflicting_filters(nats_mq_adapter: NatsMessageQueue):
-    stream_name = "just_testing"
-    await nats_mq_adapter.ensure_stream(stream_name, False, ["just_testing"])
+    queue = ConsumerQueue("just_testing")
+    await nats_mq_adapter.ensure_stream(queue)
     with pytest.raises(BadRequestError):
-        await nats_mq_adapter.ensure_stream("just_testing2", False, ["*"])
-    await nats_mq_adapter.delete_stream(stream_name)
+        queue2 = ConsumerQueue("just_testing2")
+        queue2.subjects = ["*"]
+        await nats_mq_adapter.ensure_stream(queue2)
+    await nats_mq_adapter.delete_stream(queue)
 
 
-async def test_update_stream_subject(nats_mq_adapter: NatsMessageQueue, test_stream):
-    await nats_mq_adapter.ensure_stream(test_stream, False, [str(uuid.uuid4)[:8]])
-    queue = ConsumerQueue(test_stream)
-    stream_info = await nats_mq_adapter._js.stream_info(queue.queue_name)
-    assert stream_info.config.subjects != [test_stream]
+async def test_update_stream_subject(nats_mq_adapter: NatsMessageQueue, test_queue: ConsumerQueue):
+    test_queue.subjects = [str(uuid.uuid4)[:8]]
+    await nats_mq_adapter.ensure_stream(test_queue)
+    stream_info = await nats_mq_adapter._js.stream_info(test_queue.queue_name)
+    assert stream_info.config.subjects != [test_queue.queue_name]
 
 
-async def test_update_stream_retention(nats_mq_adapter: NatsMessageQueue, test_stream):
+async def test_update_stream_retention(nats_mq_adapter: NatsMessageQueue, test_queue: ConsumerQueue):
+    test_queue.retention_policy = RetentionPolicy.LIMITS
     with pytest.raises(ServerError):
-        await nats_mq_adapter.ensure_stream(test_stream, True)
+        await nats_mq_adapter.ensure_stream(test_queue)
 
 
 async def test_ensure_consumer(test_consumer: bool):
     assert test_consumer
 
 
-async def test_ensure_consumer_with_name(nats_mq_adapter: NatsMessageQueue, test_stream: str, test_consumer):
-    await nats_mq_adapter.ensure_consumer(test_stream)
-    await nats_mq_adapter.ensure_consumer(test_stream)
+async def test_ensure_consumer_with_name(nats_mq_adapter: NatsMessageQueue, test_queue, test_consumer):
+    await nats_mq_adapter.ensure_consumer(test_queue)
+    await nats_mq_adapter.ensure_consumer(test_queue)
