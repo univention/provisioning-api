68c1f62e7605f09539b27ae1bd0aed831163652f
Johannes Lohmer <lohmer@univention.de>
2025-11-24 00:22:10 +0100

    fix(listener): Remove code duplication from the listener

--- listener/src/univention/provisioning/listener/mq_adapter_nats.py
+++ listener/src/univention/provisioning/listener/mq_adapter_nats.py
@@ -9,22 +9,18 @@
 import logging
 from datetime import datetime
 from enum import Enum
-from typing import Any, Dict, Optional, Self
+from typing import Any, Dict, Optional
 
 import msgpack
-from nats.aio.client import Client as NATS
-from nats.js.api import RetentionPolicy, StreamConfig
-from nats.js.errors import NotFoundError
 from pydantic import BaseModel, Field, root_validator
 from pydantic.json import pydantic_encoder
+
+from univention.provisioning.backends.nats_mq import LdapQueue, NatsMessageQueue
 
 from .config import LdapProducerSettings, ldap_producer_settings
 from .mq_port import MessageQueuePort
 
 logger = logging.getLogger(__name__)
-
-LDAP_SUBJECT = "ldap-producer-subject"
-LDAP_PRODUCER_QUEUE_NAME = "ldap-producer"
 
 LDAP_OBJECT_TYPE_FIELD = "univentionObjectType"
 UDM_OBJECT_TYPE_FIELD = "objectType"
@@ -86,119 +82,6 @@ def messagepack_encoder(data: Any) -> bytes:
     return msgpack.packb(data, default=pydantic_encoder)
 
 
-class NatsKeys:
-    """A list of keys used in Nats for queueing messages."""
-
-    @staticmethod
-    def stream(subject: str) -> str:
-        return f"stream:{subject}"
-
-    @staticmethod
-    def durable_name(subject: str) -> str:
-        return f"durable_name:{subject}"
-
-
-# copy of NatsMessageQueue from backends.nats_mq
-class NatsMessageQueue:
-    """
-    Message queueing using NATS.
-
-    Use as an asynchronous context manager to ensure the connection gets closed after usage.
-    """
-
-    def __init__(self, server: str, user: str, password: str, max_reconnect_attempts: int = 5, **connect_kwargs):
-        self._server = server
-        self._user = user
-        self._password = password
-        self._max_reconnect_attempts = max_reconnect_attempts
-        self._connect_kwargs = connect_kwargs
-        self._nats = NATS()
-        self._js = self._nats.jetstream()
-
-    async def __aenter__(self) -> Self:
-        await self.connect()
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb) -> bool:
-        await self.close()
-        return False
-
-    async def connect(self):
-        """
-        Connect to the NATS server.
-
-        Arguments are passed directly to the NATS client.
-        https://nats-io.github.io/nats.py/modules.html#asyncio-client
-
-        By default, it fails after a maximum of 10 seconds because of a 2 second connect timout * 5 reconnect attempts.
-        """
-        await self._nats.connect(
-            servers=self._server,
-            user=self._user,
-            password=self._password,
-            max_reconnect_attempts=self._max_reconnect_attempts,
-            error_cb=self.error_callback,
-            disconnected_cb=self.disconnected_callback,
-            closed_cb=self.closed_callback,
-            reconnected_cb=self.reconnected_callback,
-            **self._connect_kwargs,
-        )
-
-    async def error_callback(self, e):
-        logger.error("There was an error during the execution: %s", e)
-
-    async def disconnected_callback(self):
-        logger.debug("Disconnected to NATS")
-
-    async def closed_callback(self):
-        logger.debug("Closed connection to NATS.")
-
-    async def reconnected_callback(self):
-        logger.debug("Reconnected to NATS")
-
-    async def close(self):
-        await self._nats.close()
-
-    async def add_message(
-        self,
-        stream: str,
-        subject: str,
-        message: bytes,
-    ):
-        """Publish a message to a NATS subject."""
-        stream_name = NatsKeys.stream(stream)
-
-        await self._js.publish(
-            subject,
-            message,
-            stream=stream_name,
-        )
-        logger.debug(
-            "Message was published to the stream: %r with the subject: %r",
-            stream_name,
-            subject,
-        )
-
-    async def ensure_stream(self, stream: str, manual_delete: bool, subjects: Optional[list[str]] = None):
-        stream_name = NatsKeys.stream(stream)
-        stream_config = StreamConfig(
-            name=stream_name,
-            subjects=subjects or [stream],
-            retention=RetentionPolicy.LIMITS if manual_delete else RetentionPolicy.WORK_QUEUE,
-            # TODO: set to 3 after nats clustering is stable.
-            num_replicas=1,
-        )
-        try:
-            await self._js.stream_info(stream_name)
-            logger.info("A stream with the name %r already exists", stream_name)
-        except NotFoundError:
-            await self._js.add_stream(stream_config)
-            logger.info("A stream with the name %r was created", stream_name)
-        else:
-            await self._js.update_stream(stream_config)
-            logger.info("A stream with the name %r was updated", stream_name)
-
-
 class MessageQueueNatsAdapter(MessageQueuePort):
     def __init__(self, settings: Optional[LdapProducerSettings] = None):
         super().__init__(settings or ldap_producer_settings())
@@ -228,7 +111,7 @@ class MessageQueueNatsAdapter(MessageQueuePort):
             body=Body(new=new, old=old),
         )
         encoded_message = messagepack_encoder(message.dict())
-        await self.mq.add_message(LDAP_PRODUCER_QUEUE_NAME, LDAP_SUBJECT, encoded_message)
+        await self.mq.add_message(LdapQueue(), encoded_message)
 
     async def ensure_queue_exists(self) -> None:
-        await self.mq.ensure_stream(LDAP_PRODUCER_QUEUE_NAME, False, [LDAP_SUBJECT])
+        await self.mq.ensure_stream(LdapQueue())
