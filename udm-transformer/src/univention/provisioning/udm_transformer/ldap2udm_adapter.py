f8fd3ae09468a45ca47aa16b21b714e708a30288
Dirk Wiesenthal <wiesenthal@univention.de>
2025-12-11 03:07:25 +0100

    feat(udm-transformer): Use UDM REST API to transform instead of a local library
    Issue univention/dev/projects/provisioning#97

--- udm-transformer/src/univention/provisioning/udm_transformer/ldap2udm_adapter.py
+++ udm-transformer/src/univention/provisioning/udm_transformer/ldap2udm_adapter.py
@@ -1,106 +1,75 @@
 # Callable | NoneSPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2024 Univention GmbH
 
-import importlib
+import base64
 import logging
+import urllib.parse
 from typing import Optional
 
-import univention.admin.uldap
-from univention.admin.rest.module import Object
-from univention.management.console.log import MODULE
-from univention.management.console.modules.udm.udm_ldap import UDM_Module
+import dns.resolver
+import requests
+
+from univention.admin.rest.client import UDM, ServerError, UnprocessableEntity
 
 from .config import UDMTransformerSettings
 from .ldap2udm_port import Ldap2Udm
 
 logger = logging.getLogger(__name__)
 
-SUPPORTED_OBJECT_TYPES = [
-    "oxmail/accessprofile",
-    "oxmail/functional_account",
-    "oxmail/oxcontext",
-    "oxresources/oxresources",
-    "portals/all",
-    "portals/announcement",
-    "portals/category",
-    "portals/entry",
-    "portals/folder",
-    "portals/portal",
-    "portals/announcement",
-    "portals/category",
-    "portals/entry",
-    "portals/folder",
-    "portals/portal",
-    "users/user",
-    "groups/group",
-]
 
 UDM_MODULES_RELOAD_TRIGGER = {
     "settings/extended_attribute",
-    "settings/extended_options",
-    "settings/udm_hook",
-    "settings/udm_module",
-    "settings/udm_syntax",
 }
 
 
-class ModuleNotFound(Exception):
+class UdmRestApiError(Exception):
     pass
 
 
-class Ldap2UdmAdapter(Ldap2Udm, univention.admin.uldap.access):
+class Ldap2UdmAdapter(Ldap2Udm):
     def __init__(self, settings: Optional[UDMTransformerSettings] = None):
         Ldap2Udm.__init__(self, settings)
-        univention.admin.uldap.access.__init__(
-            self,
-            host=settings.ldap_host,
-            port=settings.ldap_port,
-            start_tls=2 if settings.ldap_tls_mode.lower() == "on" else 0,
-            base=settings.ldap_base_dn,
-            binddn=settings.ldap_bind_dn,
-            bindpw=settings.ldap_bind_pw,
-        )
-        self._my_ldap_position = univention.admin.uldap.position(settings.ldap_base_dn)
-        self.ldap_publisher_name = settings.ldap_publisher_name
+        self.udm_url = settings.udm_url
+        self.udm_auth = (settings.udm_username, settings.udm_password)
+        self.udm_needs_reload = settings.udm_needs_reload
+        self.udm = UDM.http(self.udm_url, *self.udm_auth)
 
-    @staticmethod
-    def reload_udm_if_required(obj: dict) -> None:
+    def discover_pods_ips(self):
+        # kubernetes magic
+        hostname = urllib.parse.urlparse(self.udm_url).hostname
+        udm_api_ips = dns.resolver.resolve(hostname, "A", search=True)
+        return [ip.address for ip in udm_api_ips]
+
+    def reload_udm_if_required(self, obj: dict) -> None:
+        if not self.udm_needs_reload:
+            return
         if obj.get("objectType") not in UDM_MODULES_RELOAD_TRIGGER:
             return
         logger.info("Reload of UDM modules triggered by change of %r object.", obj["objectType"])
-        importlib.reload(univention.management.console.modules.udm.udm_ldap)
-
-    def _get_module(self, object_type) -> UDM_Module:
-        module = UDM_Module(object_type, ldap_connection=self, ldap_position=self._my_ldap_position)
-        if not module or not module.module:
-            raise ModuleNotFound
-        return module
+        for ip in self.discover_pods_ips():
+            # ATTENTION: credentials via HTTP. okay as this is meant to be done only inside "kubernetes VPN"
+            (
+                requests.get(
+                    "http://%s/univention/udm/-/reload" % ip, auth=self.udm_auth, headers={"Accept": "application/json"}
+                ),
+            )
 
     def ldap_to_udm(self, entry: dict) -> dict:
-        object_types = entry.get("univentionObjectType", [])
-        if not isinstance(object_types, list) or len(object_types) < 1:
-            MODULE.warn("ReadControl response is missing `univentionObjectType`!")
-            return {}
+        dn = entry["entryDN"][0].decode("utf-8")
+        # the UDM REST API expects only base64 encoded attributes
+        attributes = {k: [base64.b64encode(_v).decode("utf-8") for _v in v] for k, v in entry.items()}
+        payload = {
+            "dn": dn,
+            "attributes": attributes,
+        }
 
-        object_type = object_types[0].decode("utf-8")
         try:
-            module = self._get_module(object_type)
-            module_obj = module.module.object(
-                co=None,
-                lo=self,
-                position=self._my_ldap_position,
-                dn=entry["entryDN"][0],
-                superordinate=None,
-                attributes=entry,
+            return self.udm.client.request(
+                "POST", self.udm.uri + "/directory/unmap-ldap-attributes", data=payload, expect_json=True
             )
-            module_obj.open()
-            return Object.get_representation(module, module_obj, ["*"], self, False)
-        except ModuleNotFound:
-            MODULE.error("ReadControl response has object type %r, but the module was not found!" % object_type)
-            return {}
-        except Exception:
-            if object_type in SUPPORTED_OBJECT_TYPES:
-                raise
-            logger.exception(
-                "Ignoring failed transformation of unsupported UDM type %r. Object: %r", object_type, entry
-            )
+        except UnprocessableEntity as exc:
+            logger.error(str(exc))
+            raise UdmRestApiError("ReadControl response suggests that UDM does not recognize the object!")
+        except ServerError as exc:
+            logger.error(str(exc))
+            raise UdmRestApiError("ReadControl response points to a server error!")
