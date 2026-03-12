d99a0f6ecc4b1c89398bae9022c7f6c4162319a6
Jürn Brodersen <brodersen@univention.de>
2026-03-12 23:05:12 +0100

    feat(app-release): add custom jinja filter to include raw files

--- images/app-release/render_appcenter_files.sh
+++ images/app-release/render_appcenter_files.sh
@@ -4,7 +4,7 @@ for appcenter_file_template in "$APPCENTER_FILE_DIR"/*.jinja; do
   DESTINATION="$APPCENTER_FILE_DIR/$(basename "$appcenter_file_template" ".jinja")"
   echo "Rendering $appcenter_file_template: "
   cat "$appcenter_file_template"
-  jinjanate --quiet "$appcenter_file_template" --output-file "$DESTINATION"
+  jinjanate --filters /var/lib/jinja_source_file.py --quiet "$appcenter_file_template" --output-file "$DESTINATION"
   echo "Render result: "
   cat "$DESTINATION"
 done
